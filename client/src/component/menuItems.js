bde70a5bfcedf26fa0fbdff107e299857b8cad1c
Asifzia007 <asif.ziakps@gmail.com>
2024-06-03 14:27:25 +0530

    first repo

---
+++ client/src/component/menuItems.js
@@ -0,0 +1,10 @@
+// menuItems.js
+import React from "react";
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+
+
+
+export const menuItems = [
+  { text: "Dashboard", icon: <LeaderboardIcon  />, path: "/dashboard" },
+
+];
